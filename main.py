23844aa66f70670ac1f4f3c2128ff8fa9c2754ad
Mehdi Majid <mmajid@bisk.edu.krd>
2025-09-29 15:32:13 +0300

    2nd: working word_to_pdf_convertor.

--- main.py
+++ main.py
@@ -14,33 +14,41 @@ def convert_pdf_to_docx(input_folder, output_folder=None):
     :param output_folder:
     :return:
     """
-    # if output_folder and not os.path.exists(output_folder):
-    #     os.makedirs(output_folder)
-    #     print(f"Created folder for:  {output_folder}")
+    if output_folder and not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+        print(f"Created folder for:  {output_folder}")
     n = 0
     for root, dirs, files in os.walk(input_folder):
-        print(f"root: {root}, dirs: {dirs}, files: {files}, len(files)={len(files)}, ")
-        print(f"{root} {len(files)}")
+        # print(f"root: {root}, dirs: {dirs}, files: {files}, len(files)={len(files)}, ")
+        # print(f"{root} {len(files)}")
         if n >= 0:
             try:
-                # result = subprocess.run(['tree',"/A", f"{root}"], capture_output=True, text=True, check=True, shell=True)
                 result = subprocess.run(['tree',"/f", f"{root}"], capture_output=True, text=True, check=True, shell=True)
                 print(result.stdout)
             except FileNotFoundError:
                 print("No such file or directory")
         n+=1
-        # print((subprocess.run(['tree "{root}"'], capture_output=True, text=True, check=True, shell=True)).stdout)
+    for file_name in os.listdir(input_folder):
+        # if file_name.endswith(".docx"):
+        if file_name.lower().endswith(".docx"):
+            docx_path = os.path.join(input_folder, file_name)
+        #     print(f"Converting: {docx_path}")
+        # print(f"abspath  => {os.path.abspath(file_name)}")
+            if output_folder:
+                pdf_filename = file_name.replace(".docx", ".pdf")
+                pdf_path = os.path.join(output_folder, pdf_filename)
+            else:
+                pdf_path = docx_path.replace(".docx", ".pdf")
+        try:
+            convert(docx_path, pdf_path)
+            print(f"Converted {pdf_path}")
+        except subprocess.CalledProcessError as e:
+            print(e)
 
 
 # convert_pdf_to_docx(input_folder="C:\\Users\\MehdiMajid\\Documents")
-convert_pdf_to_docx(input_folder=Path("C:/Users/MehdiMajid/Documents"))
-# rooti = Path("C:/Users/MehdiMajid/Documents/")
-# print(rooti)
-# print(rooti.exists())
-# print(rooti.is_dir())
-# print(type(rooti))
-# print((subprocess.run(["tree", r"C:/Users/MehdiMajid/Documents/"], capture_output=True, text=True, check=True, shell=True)).stdout)
-# print((subprocess.run(["tree", f"{rooti}"], capture_output=True, text=True, check=True, shell=True)).stdout)
+convert_pdf_to_docx(input_folder=Path("C:/Users/MehdiMajid/Documents/WordFiles"))
+
 
 def __main__():
     # convert_pdf_to_docx("C:\\Users\\MehdiMajid\\Document")
